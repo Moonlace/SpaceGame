0e188f0cdc9081293dce7feb1b3ed048e2b434bc
Ricardo Jesus Silva <ricardo.jesus.silva@novabase.pt>
2013-12-07 20:25:30 +0000

    created CCParallaxNodeExtras.cpp, created infinite background aded movement to the ship by tilting the screen

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -38,6 +38,38 @@ bool HelloWorld::init()
     CCSize winSize = CCDirector::sharedDirector()->getWinSize();
     _ship->setPosition(ccp(winSize.width * 0.1, winSize.height * 0.5));
     _batchNode->addChild(_ship, 1);
+    
+    // 1) Create the CCParallaxNode
+    _backgroundNode = CCParallaxNodeExtras::node(); //1
+    this->addChild(_backgroundNode,-1);
+    
+    // 2) Create the sprites will be added to the CCParallaxNode
+    _spacedust1 = CCSprite::create("bg_front_spacedust.png");
+    _spacedust2 = CCSprite::create("bg_front_spacedust.png");
+    _planetsunrise = CCSprite::create("bg_planetsunrise.png");
+    _galaxy = CCSprite::create("bg_galaxy.png");
+    _spacialanomaly = CCSprite::create("bg_spacialanomaly.png");
+    _spacialanomaly2 = CCSprite::create("bg_spacialanomaly2.png");
+    
+    // 3) Determine relative movement speeds for space dust and background
+    CCPoint dustSpeed = ccp(0.1, 0.1);
+    CCPoint bgSpeed = ccp(0.05, 0.05);
+    
+    // 4) Add children to CCParallaxNode
+    _backgroundNode->addChild(_spacedust1, 0, dustSpeed, ccp(0,winSize.height/2) ); // 2
+    _backgroundNode->addChild(_spacedust2, 0, dustSpeed, ccp( _spacedust1->getContentSize().width,winSize.height/2));
+    _backgroundNode->addChild(_galaxy, -1, bgSpeed, ccp(0, winSize.height * 0.7));
+    _backgroundNode->addChild(_planetsunrise, -1 , bgSpeed, ccp(600, winSize.height * 0));
+    _backgroundNode->addChild(_spacialanomaly, -1, bgSpeed, ccp(900, winSize.height * 0.3));
+    _backgroundNode->addChild(_spacialanomaly2, -1, bgSpeed, ccp(1500, winSize.height * 0.9));
+    this->scheduleUpdate();
+    
+    HelloWorld::addChild(CCParticleSystemQuad::create("Stars1.plist"));
+    HelloWorld::addChild(CCParticleSystemQuad::create("Stars2.plist"));
+    HelloWorld::addChild(CCParticleSystemQuad::create("Stars3.plist"));
+    
+    this->setAccelerometerEnabled(true);
+    
     return true;
 }
 
@@ -52,4 +84,63 @@ void HelloWorld::menuCloseCallback(CCObject* pSender)
     exit(0);
 #endif
 #endif
+}
+
+void HelloWorld::update(float dt) {
+    CCPoint backgroundScrollVert = ccp(-1000, 0);
+    _backgroundNode->setPosition(ccpAdd(_backgroundNode->getPosition(), ccpMult(backgroundScrollVert, dt)));
+    
+    CCArray *spaceDusts = CCArray::createWithCapacity(2);
+    spaceDusts->addObject(_spacedust1);
+    spaceDusts->addObject(_spacedust2);
+    for ( int ii = 0; ii <spaceDusts->count(); ii++ ) {
+        CCSprite * spaceDust = (CCSprite *)(spaceDusts->objectAtIndex(ii));
+        float xPosition = _backgroundNode->convertToWorldSpace(spaceDust->getPosition()).x;
+        float size = spaceDust->getContentSize().width;
+        if ( xPosition < -size/2 ) {
+            _backgroundNode->incrementOffset(ccp(spaceDust->getContentSize().width*2,0),spaceDust);
+        }
+    }
+    
+    CCArray *backGrounds = CCArray::createWithCapacity(4);
+    backGrounds->addObject(_galaxy);
+    backGrounds->addObject(_planetsunrise);
+    backGrounds->addObject(_spacialanomaly);
+    backGrounds->addObject(_spacialanomaly2);
+    for ( int ii = 0; ii <backGrounds->count(); ii++ ) {
+        CCSprite * background = (CCSprite *)(backGrounds->objectAtIndex(ii));
+        float xPosition = _backgroundNode->convertToWorldSpace(background->getPosition()).x;
+        float size = background->getContentSize().width;
+        if ( xPosition < -size ) {
+            _backgroundNode->incrementOffset(ccp(2000,0),background); 
+        }
+    }
+    
+    CCSize winSize = CCDirector::sharedDirector()->getWinSize();
+    float maxY = winSize.height - _ship->getContentSize().height/2;
+    float minY = _ship->getContentSize().height/2;
+    
+    float diff = (_shipPointsPerSecY * dt);
+    float newY = _ship->getPosition().y + diff;
+    newY = MIN(MAX(newY, minY), maxY);
+    _ship->setPosition(ccp(_ship->getPosition().x, newY));
+}
+
+void HelloWorld::didAccelerate(CCAcceleration* pAccelerationValue) {
+#define KFILTERINGFACTOR 0.1
+#define KRESTACCELX -0.6
+#define KSHIPMAXPOINTSPERSEC (winSize.height*0.5)
+#define KMAXDIFFX 0.2
+    
+    double rollingX;
+    
+    // Cocos2DX inverts X and Y accelerometer depending on device orientation
+    // in landscape mode right x=-y and y=x !!! (Strange and confusing choice)
+    pAccelerationValue->x = pAccelerationValue->y;
+    rollingX = (pAccelerationValue->x * KFILTERINGFACTOR) + (rollingX * (1.0 - KFILTERINGFACTOR));
+    float accelX = pAccelerationValue->x - rollingX;
+    CCSize winSize = CCDirector::sharedDirector()->getWinSize();
+    float accelDiff = accelX - KRESTACCELX;
+    float accelFraction = accelDiff / KMAXDIFFX;
+    _shipPointsPerSecY = KSHIPMAXPOINTSPERSEC * accelFraction;
 }
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -2,20 +2,37 @@
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
+#include "CCParallaxNodeExtras.h"
+
+USING_NS_CC;
 
 class HelloWorld : public cocos2d::CCLayer
 {
 
 private:
-	cocos2d::CCSpriteBatchNode * _batchNode;
-	cocos2d::CCSprite * _ship;
+	CCSpriteBatchNode * _batchNode;
+	CCSprite * _ship;
+    CCParallaxNodeExtras *_backgroundNode;
+    CCSprite *_spacedust1;
+    CCSprite *_spacedust2;
+    CCSprite *_planetsunrise;
+    CCSprite *_galaxy;
+    CCSprite *_spacialanomaly;
+    CCSprite *_spacialanomaly2;
+    
+    float _shipPointsPerSecY;
+    
+    // scheduled Update
+    void update(float dt);
 
 public:
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();  
-
+    
+    virtual void didAccelerate(CCAcceleration* pAccelerationValue);
+    
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
-    static cocos2d::CCScene* scene();
+    static CCScene* scene();
     
     // a selector callback
     void menuCloseCallback(CCObject* pSender);
